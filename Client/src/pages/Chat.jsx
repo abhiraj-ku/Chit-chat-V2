b554d77fe75611cf8d940dae36b77bf04902fd41
abhishek <abhirajabhi312@gmail.com>
2023-10-28 23:05:40 +0530

    started adding chat and contact component

--- Client/src/pages/Chat.jsx
+++ Client/src/pages/Chat.jsx
@@ -1,5 +1,67 @@
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoutes.js";
+import axios from "axios";
+import Contacts from "../components/Contacts";
 function Chat() {
-  return <div>Chat App v2</div>;
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+
+  //getting user from localStorage session
+  useEffect(() => {
+    if (!localStorage.getItem("chit-chat-user")) {
+      navigate("/login");
+    } else {
+      setCurrentUser(JSON.parse(localStorage.getItem("chit-chat-app")));
+    }
+  }, []);
+
+  //api calling
+  useEffect(() => {
+    if (currentUser) {
+      if (currentUser.isAvatarImageSet) {
+        const data = axios.get(`${allUsersRoute}/${currentUser._id}`);
+        setContacts(data.data);
+      } else {
+        navigate("/setAvatar");
+      }
+    }
+  }, []);
+  return (
+    <Container>
+      <div className="container">
+        <Contacts contacts={contacts} currentUser={currentUser} />
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+
+    @media screen and (max-width: 719px) {
+      grid-template-columns: 100%;
+    }
+  }
+`;
 
 export default Chat;
--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -55,3 +55,19 @@ module.exports.setAvatar = async (req, res, next) => {
     next(error);
   }
 };
+
+//allusers api route
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};

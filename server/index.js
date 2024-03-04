1d19ad38ce67bfbc1014b59f52ddc26aa0ff5904
Abhishek Kumar <abhirajabhi312@gmail.com>
2024-03-04 12:38:56 +0530

    adding google oAuth support

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const app = express();
 const cors = require("cors");
 const helmet = require("helmet");
 const express = require("express");
-const mongoose = require("mongoose");
 const passport = require("passport");
+const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const userRoutes = require("./routes/userRoutes");
 const messagesRoute = require("./routes/messagesRoute");
+const { Strategy } = require("passport-google-oauth20");
+const User = require("./model/userModel");
 
 const socket = require("socket.io");
 
@@ -17,11 +19,42 @@ app.use(express.json());
 
 // google Oauth config
 const googleConfig = {
-  CLIENT_ID,
-  CLIENT_SECRET,
-  COOKIE_KEY,
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY: process.env.COOKIE_KEY,
 };
 
+// auth options
+const AUTH_OPTIONS = {
+  callbackURL: "http://localhost:3000/auth/google/callback",
+  clientID: googleConfig.CLIENT_ID,
+  clientSecret: googleConfig.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log("Google profile", profile);
+
+  //error handling if profile is null
+  if (!profile) {
+    return done(new Error("Authentication failed"));
+  }
+
+  done(null, profile);
+}
+
+// passport serialize
+passport.serializeUser((user, done) => {
+  done(nulll, user.id);
+});
+passport.deserializeUser((user, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+  // done(nulll, user.id);
+});
+
+//
+passport.use(new Strategy());
 // Root route with a beautiful welcome message
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/welcome.html");
